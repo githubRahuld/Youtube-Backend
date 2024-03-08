98b99d9d163ac1f60080ed0ad690590b805eed60
Rahul Dhakad <rahuldhakad201.rd@gmail.com>
2024-03-08 18:31:03 +0530

    add subscription controller

---
+++ src/models/subscription.models.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema, mongo } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      typeof: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      typeof: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
