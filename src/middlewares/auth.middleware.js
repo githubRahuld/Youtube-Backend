8ef520f1732f9e78e3bdef0469e2ee80bd485419
Rahul Dhakad <rahuldhakad201.rd@gmail.com>
2024-03-09 18:27:04 +0530

    add new user routes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,6 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.models.js";
 
+// to check user is logged in
 const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  createNewPassword,
+  getCurrentuser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAvatar,
+  updateCoverImage,
+  updateUserDetails,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -32,5 +39,23 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, createNewPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentuser);
+
+// patch: used tp update espacific data
+router.route("/update-account").patch(verifyJWT, updateUserDetails);
+
+// upload avatar using multer(upload) which is single picture
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
